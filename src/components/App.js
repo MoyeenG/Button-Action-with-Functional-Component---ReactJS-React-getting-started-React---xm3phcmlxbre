90111720ae71e832c9ba36f2a1f57ee3ff9d1cef
moyeen_g <moyeengodyari@gmail.com>
2023-02-07 23:07:51 +0530

    fifth commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+
+let paraId = "para";
+
+function ParaDisplay() {
+  
+  return (
+    <p id={ paraId }>Hello, 
+      I've learnt to use the full-stack evaluation tool. 
+      This makes me so happy
+    </p>  
+  );
+}
+
+
 function App() {
+  const [text,setText] = useState(); 
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={() =>setText(ParaDisplay) }>Click Me!</button>
+      {/* <ParaDisplay /> */}
+      {text}
     </div>
   );
 }
